170ce0d1b155bae0bf6756406956dcf38f371d29
Damian Szymczuk <damian.szymczuk@comarch.pl>
2014-06-10 17:28:02 +0200

    create view for event

--- single_pages/dashboard/event_calendar/event.php
+++ single_pages/dashboard/event_calendar/event.php
@@ -1,76 +1,67 @@
 <?php defined('C5_EXECUTE') or die('Access denied.');
+$form = Loader::helper('form');
 ?>
 
 <?php echo Loader::helper('concrete/dashboard')->getDashboardPaneHeaderWrapper(t('Event Calendar')); ?>
     <h3><?php echo t('Add / edit event') ?></h3>
 
 
-    <form method="get" action="<?php  echo $this->action('view')?>" id="ccm-multilingual-page-report-form" class="form-stacked">
-        <div class="row">
-            <fieldset class="span4">
-                <legend><?php  echo t('Choose Source')?></legend>
-                <div class="clearfix">
-                    <div class="input" style="margin-top: 15px;">
-                        <?php  echo $form->select('sectionIDSelect', $sections, $sectionID)?>
-                    </div>
+    <div class="btn-group" style="margin-top: 15px;">
+        <a class="btn" href="<?php echo View::url('dashboard//event_calendar/list_event') ?>">Return to event list</a>
+
+    </div>
+
+    <form method="post" id="ccm-multilingual-page-report-form" style="margin-top: 35px;">
+        <fieldset>
+            <label><?php echo t('Event title') ?></label>
+            <div style="margin-top: 15px;">
+                <input type="text" name="event_title" id="event_title" value="<?php echo $event_title; ?>">
+            </div>
+        </fieldset>
+
+
+        <fieldset>
+            <label><?php echo t('Event date') ?></label>
+            <div style="margin-top: 15px;">
+                <input type="text" name="event_date" id="event_date" value="<?php echo $event_date; ?>">
+            </div>
+        </fieldset>
+        <fieldset>
+            <label><?php echo t('Event type') ?></label>
+            <div style="margin-top: 15px;">
+                <input type="text" name="event_type" id="event_type" value="<?php echo $event_type; ?>">
+            </div>
+        </fieldset>
+        <fieldset>
+            <label><?php echo t('Event description') ?></label>
+            <div style="margin-top: 15px;">
+                <input type="text" name="event_description" id="event_description" value="<?php echo $event_description; ?>">
+            </div>
+        </fieldset>
+        <fieldset>
+            <label><?php echo t('Event url') ?></label>
+            <div style="margin-top: 15px;">
+                <input type="text" name="event_url" id="event_url" value="<?php echo $event_url; ?>">
+            </div>
+        </fieldset>
+
+
+        <fieldset>
+            <div class="clearfix">
+                <div style="margin-top: 10px;">
+                    <input class="btn" type="submit" value="<?php echo t('Add event') ?>">
+
                 </div>
-            </fieldset>
-            <fieldset class="span4">
-                <legend><?php  echo t('Choose Targets')?></legend>
-                <div class="clearfix">
-                    <?php   foreach($sectionList as $sc) { ?>
-                        <?php   $args = array('style' => 'vertical-align: middle');
-                        if ($sectionID == $sc->getCollectionID()) {
-                            $args['disabled'] = 'disabled';
-                        }
-                        ?>
-                        <div class="input">
-                            <ul class="inputs-list">
-                                <li>
-                                    <label>
-                                        <?php  echo $form->checkbox('targets[' . $sc->getCollectionID() . ']', $sc->getCollectionID(), in_array($sc->getCollectionID(), $targets), $args)?>
-                                        <span>
-										<?php  echo $fh->getSectionFlagIcon($sc)?>
-                                        <?php  echo $sc->getLanguageText(). " (".$sc->getLocale().")"; ?>
-									</span>
-                                    </label>
-                                </li>
-                            </ul>
-                        </div>
-                    <?php   } ?>
-                </div>
-            </fieldset>
-            <fieldset class="span4">
-                <legend><?php  echo t('Display')?></legend>
-                <div class="clearfix">
-                    <div class="input">
-                        <ul class="inputs-list">
-                            <li>
-                                <label>
-                                    <?php  echo $form->radio('showAllPages', 0, 0)?>
-                                    <span><?php   echo t('Only Missing Pages')?></span>
-                                </label>
-                            </li>
-                            <li>
-                                <label>
-                                    <?php  echo $form->radio('showAllPages', 1, false)?>
-                                    <span><?php   echo t('All Pages') ?></span>
-                                </label>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </fieldset>
-            <fieldset class="span2">
-                <div class="clearfix">
-                    <div class="input" style="margin-top: 10px;">
-                        <?php  echo $form->submit('submitForm', t('Go'))?>
-                        <?php  echo $form->hidden('sectionID', $sectionID); ?>
-                    </div>
-                </div>
-            </fieldset>
-        </div>
+            </div>
+        </fieldset>
     </form>
 
+<script>
+    $(document).ready(function(){
+        $('#event_date').datepicker();
+    });
+</script>
 
 <?php echo Loader::helper('concrete/dashboard')->getDashboardPaneFooterWrapper(); ?>
+
+

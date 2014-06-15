c07c634f49e51c71080608fa86f4484b8c071333
Damian <damian.strony@gmail.com>
2014-06-15 20:55:04 +0200

    delete unused code

--- blocks/event_calendar/add.php
+++ blocks/event_calendar/add.php
@@ -2,11 +2,6 @@
 defined('C5_EXECUTE') or die(_("Access Denied."));
 ?>
 
-<input type="text" name="calendarID" value="<?php echo $calendarID; ?>"/>
-
-<?php echo $addval ?>
-
-
 <label for="calendarID"><?php echo t('Calendar') ?></label>
 
 <div style="margin-top: 15px;">

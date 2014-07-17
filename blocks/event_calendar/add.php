f873641c5853ae4f158ce007cf81f14c613b190d
Damian Szymczuk <damian.szymczuk@comarch.pl>
2014-07-17 11:16:19 +0200

    include setup form in block

--- blocks/event_calendar/add.php
+++ blocks/event_calendar/add.php
@@ -1,21 +1,4 @@
 <?php
 defined('C5_EXECUTE') or die(_("Access Denied."));
+$bt->inc('form_setup_html.php', array('calendars' => $calendars));
 ?>
-
-<?php if (empty($calendars)): ?>
-    <div class="alert alert-info">
-        <?php echo t('No calendar exist. Add from one from dashboard.') ?>
-    </div>
-<?php else: ?>
-    <div style="margin-top: 15px;">
-        <label for="calendarID"><?php echo t('Select calendar') ?></label>
-
-        <div style="margin-top: 15px;">
-            <select name="calendarID" id="calendarID">
-                <?php foreach ($calendars as $cal): ?>
-                    <option value="<?php echo $cal['calendarID'] ?>"><?php echo $cal['title'] ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-    </div>
-<?php endif; ?>
--- blocks/event_calendar/edit.php
+++ blocks/event_calendar/edit.php
@@ -1,24 +1,4 @@
 <?php
 defined('C5_EXECUTE') or die(_("Access Denied."));
+$bt->inc('form_setup_html.php', array('calendars' => $calendars,'calendarID' => $calendarID));
 ?>
-
-<?php if (empty($calendars)): ?>
-    <div class="alert alert-info">
-        <?php echo t('No calendar exist. Add from one from dashboard.') ?>
-    </div>
-<?php else: ?>
-    <div style="margin-top: 15px;">
-        <fieldset>
-            <label for="calendarID"><?php echo t('Select calendar') ?></label>
-
-            <div style="margin-top: 15px;">
-                <select name="calendarID" id="calendarID">
-                    <?php foreach ($calendars as $cal): ?>
-                        <option
-                            value="<?php echo $cal['calendarID'] ?>" <?php if ($calendarID == $cal['calendarID']) echo "selected" ?>><?php echo $cal['title'] ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
-        </fieldset>
-    </div>
-<?php endif; ?>

54267c31ce48b756e9dab94205be77ab558a7230
Damian <damian.strony@gmail.com>
2014-12-21 00:32:04 +0100

    prepare frontend to full calendar - at now work only example

--- blocks/event_calendar/view.php
+++ blocks/event_calendar/view.php
@@ -21,7 +21,79 @@ $c = Page::getCurrentPage();
 <?php if (!$c->isEditMode()): ?>
     <script>
         $(document).ready(function () {
-            var eventsInline = {};
+
+            console.log(<?php echo $events; ?>);
+
+            $("#eventCalendarInline<?php echo $blockIdentifier; ?>").fullCalendar({
+                defaultDate: '2014-11-12',
+                editable: true,
+                eventClick: function(calEvent, jsEvent, view) {
+                    console.log(calEvent);
+                    console.log(jsEvent);
+                    console.log(view);
+                    $(this).css('border-color', 'red');
+                },
+                eventLimit: true, // allow "more" link when too many events
+                events: [
+                    {
+                        title: 'All Day Event',
+                        start: '2014-11-01'
+                    },
+                    {
+                        title: 'Long Event',
+                        start: '2014-11-07',
+                        end: '2014-11-10'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: '2014-11-09T16:00:00'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: '2014-11-16T16:00:00'
+                    },
+                    {
+                        title: 'Conference',
+                        start: '2014-11-11',
+                        end: '2014-11-13',
+                        dupka: "dupcyngowo"
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2014-11-12T10:30:00',
+                        end: '2014-11-12T12:30:00'
+                    },
+                    {
+                        title: 'Lunch',
+                        start: '2014-11-12T12:00:00'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2014-11-12T14:30:00'
+                    },
+                    {
+                        title: 'Happy Hour',
+                        start: '2014-11-12T17:30:00'
+                    },
+                    {
+                        title: 'Dinner',
+                        start: '2014-11-12T20:00:00'
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: '2014-11-13T07:00:00'
+                    },
+                    {
+                        title: 'Click for Google',
+                        url: 'http://google.com/',
+                        start: '2014-11-28'
+                    }
+                ]
+            });
+
+            /*var eventsInline = {};
             eventsInline = <?php echo $events; ?>;
             var settings = {};
             var set_serv = <?php echo $settings; ?>;
@@ -41,7 +113,7 @@ $c = Page::getCurrentPage();
                 eventsInDay: settings.eventsInDay,
                 closeText: settings.closeText,
                 typeText: settings.typeText
-            });
+            });*/
         });
     </script>
 <?php endif ?>

4273877448d19a0e421a8742ad094b23cc4228e2
Damian <damian.strony@gmail.com>
2015-06-05 17:02:21 +0200

    release 3.3 version - Filter calendar events by single type

--- controller.php
+++ controller.php
@@ -7,7 +7,7 @@ class dsEventCalendarPackage extends Package
 
     protected $pkgHandle = 'dsEventCalendar';
     protected $appVersionRequired = '5.5.0';
-    protected $pkgVersion = '3.2.2';
+    protected $pkgVersion = '3.3';
 
     public function getPackageDescription()
     {

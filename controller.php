231c21a9d559bbc2aa456353f69d3dcbca69dab8
Damian <damian.strony@gmail.com>
2015-10-20 21:20:12 +0200

    release 3.3.4

--- controller.php
+++ controller.php
@@ -7,7 +7,7 @@ class dsEventCalendarPackage extends Package
 
     protected $pkgHandle = 'dsEventCalendar';
     protected $appVersionRequired = '5.5.0';
-    protected $pkgVersion = '3.3.3';
+    protected $pkgVersion = '3.3.4';
 
     public function getPackageDescription()
     {
